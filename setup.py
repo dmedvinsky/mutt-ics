c3480586c6511a6289347d7b2af18a58817165dd
Dmitry Medvinsky <me@dmedvinsky.name>
2016-12-23 12:12:56 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def requirements():
 
 
 setup(name='mutt_ics',
-      version='0.7',
+      version='0.8',
       description='A tool to show calendar event details in Mutt.',
       long_description=readme(),
       classifiers=[
