2b153fe7442223d719af3b8b48628b5d8673fdf0
Dmitry Medvinsky <me@dmedvinsky.name>
2019-03-27 12:49:52 +0300

    Include samples in distribution

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def requirements():
 
 
 setup(name='mutt_ics',
-      version='0.9',
+      version='0.9.1',
       description='A tool to show calendar event details in Mutt.',
       long_description=readme(),
       classifiers=[
